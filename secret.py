7d32c00620586a0ccb9d528ebc7d9a0968a026e5
Beni <75453780+benib3@users.noreply.github.com>
2022-11-03 17:26:19 +0100

    initial commit

---
+++ secret.py
@@ -0,0 +1,5 @@
+key = 'nAI8MZsAF15lxgW4EaAmDA4Mn7xxw2RQSoF4tx6A6k62jjHZU2'
+secret = 'uvjp0BX62LAS0yIupvzmCFQQGEy27XpwbaDwmTi96XabIF6Yds'
+
+key2 = 'LHifiRwnryFk2cE81apCDnOJKiLrC1MG323mRGnhcUDzZcYwjz'
+secret2 = 'Faf0sWgGm5pvjhEbe3yWsx5JQJuYEiyRHkIPuLBpOyN4OxREgI'
